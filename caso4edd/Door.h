0cdc2f164c87bb48696ef34cfcddca5b58b037dc
sarah03qc <prisilla2003@gmail.com>
2022-10-15 15:45:43 -0600

    implementacion 66% de mina en puertas

--- caso4edd/Door.h
+++ caso4edd/Door.h
@@ -9,25 +9,35 @@ using namespace std;
 
 #define DOOR 1
 
-
 class Door {
     private: 
         int numDoorsConect = 0;
         List<Door> *listadoors;
+        bool tieneMina = false;
+        int minePosibilities = 0;
+        int random;
 
     public:
         Door() {
             listadoors = new List<Door>();
+            
+            this->generarMinaPosibilidad();
+        }
+
+        void generarMinaPosibilidad() {
+            //al crearse, se debe medir el 66% de si cuenta con una mina o no
+            minePosibilities = rand() % (100 - 0) + 0;
+            if(minePosibilities <= 66) {
+                tieneMina = true;
+            }
         }
 
         int generarConexiones(int nCreando) {
             if(nCreando == 0) {
                 return 0;
             }
-            srand((unsigned) time(NULL)); 
             numDoorsConect = rand() % (5 - 2) + 2;
 
-/*
             while(nCreando - numDoorsConect < 0) {
                 numDoorsConect = rand() % (5 - 2) + 2;
                 if(nCreando == 1) {
@@ -41,7 +51,6 @@ class Door {
                     }
                 }
             }
-*/
             nCreando = nCreando - numDoorsConect;
      
             for(int i = 0; i < numDoorsConect; i++) {
@@ -50,15 +59,12 @@ class Door {
             }
 
             int i = 0;
-            
             while(i < nCreando) {
                 //mientras estamos en el rango de las puertas que falta de crear
                 //generamos numero aleatorio (hara de index) para ir creando en las puertas afiliadas a esta 
-                srand((unsigned) time(NULL)); 
                 int index = rand() % (listadoors->getSize() - 0) + 0;
                 nCreando = listadoors->find(index)->generarConexiones(nCreando);
             }
-            
             return nCreando;
         }
 
@@ -72,6 +78,10 @@ class Door {
 
         int getConectionsNum() {
             return numDoorsConect;
+        }
+
+        bool hasMine() {
+            return tieneMina;
         }
 };
 
--- caso4edd/main.cpp
+++ caso4edd/main.cpp
@@ -7,24 +7,34 @@ using namespace std;
 
 
 int main() {
-
+    srand((unsigned) time(NULL));
     Door* puerta = new Door();
-    cout << "Faltan: " << puerta->generarConexiones(3) << endl;
+    
+    cout << "Faltan: " << puerta->generarConexiones(10) << endl;
+    cout << "Puerta principal tiene mina: " << puerta->hasMine() << endl;
     cout << "Se generaron: " << puerta->getConectionsNum() << " conexiones a la puerta inicial" << endl;
 
 
     for(int i = 0; i < puerta->getConnectedDoors()->getSize(); i++) {
         cout << "Puerta " << i + 1 << " conectada a la inicial tiene: " << puerta->getConnectedDoors()->find(i)->getConectionsNum() << " conectadas" << endl;
+        cout << "Tiene mina: " << puerta->getConnectedDoors()->find(i)->hasMine() << endl;
         
         if(puerta->getConnectedDoors()->find(i)->getConectionsNum() != 0) {
             cout << "Entrando a revisar puerta " << i + 1 << endl;
+            
             for(int j = 0; j < puerta->getConnectedDoors()->find(i)->getConectionsNum(); j++) {
                 cout << "Puerta interna conectada tiene: " << puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->getConectionsNum() << " conexiones" << endl;
+                cout << "Tiene mina: " << puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->hasMine() << endl;
+                if(puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->getConectionsNum() != 0) {
+                    cout << "Entrando a revisar puerta " << j + 1 << endl;
+                    for(int p = 0; p < puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->getConectionsNum(); p ++) {
+                        cout << "Puerta sub-interna conectada tiene: " << puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->getConnectedDoors()->find(p)->getConectionsNum() << " conexiones" << endl;
+                        cout << "Tiene mina: " << puerta->getConnectedDoors()->find(i)->getConnectedDoors()->find(j)->getConnectedDoors()->find(p)->hasMine() << endl;
+                    }
+                }
             }
         }
-        
     }
- 
 
     return 0;
 }
